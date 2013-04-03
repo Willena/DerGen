f4861b1b65fef6225713054d1a6cc4b788e650a7
Guillaume Villena <villenagui.free.fr@gmail.com>
2013-04-03 18:21:45 +0200

    Merge branch 'master' of J:\portableApp\GitPortable\Data\home\project\DerGen with conflicts.

--- js/js.js
+++ js/js.js
@@ -1,11 +1,11 @@
-tabBoule = [], tabListBallByColors = [] , ra = Raphael("holder", "1000", "400"), nbGen = 0;
+tabBoule = [], tabListBallByColors = [] , ra = Raphael("holder", "1000", "400"), nbGen = 0, values = [];
 
 Raphael.fn.ball = function (x, y, r, hue) {
     hue = hue / 10 || 0;
     return this.set(//code permettant la création d'une balle
-        //this.ellipse(x, y + r - r / 5, r, r / 2).attr({fill:"rhsb(" + hue + ", 1, .25)-hsb(" + hue + ", 1, .25)", stroke:"none", opacity:0}),
-        this.ellipse(x, y, r, r).attr({fill:"r(.6,.4)hsb(" + hue + ", 1, .85)-hsb(" + hue + ", .5, .4)", stroke:"none"})//,
-        //this.ellipse(x, y, r - r / 5, r - r / 20).attr({stroke:"none", fill:"r(.5,.1)#ccc-#ccc", opacity:0})
+       // this.ellipse(x, y + r - r / 5, r, r / 2).attr({fill:"rhsb(" + hue + ", 1, .25)-hsb(" + hue + ", 1, .25)", stroke:"none", opacity:0}),
+        this.ellipse(x, y, r, r).attr({fill:"r(.5,.9)hsb(" + hue + ", 1, .75)-hsb(" + hue + ", .5, .25)", stroke:"none"}),
+        this.ellipse(x, y, r - r / 5, r - r / 20).attr({stroke:"none", fill:"r(.5,.1)#ccc-#ccc", opacity:0})
     );
 };
 Raphael.fn.createBocal = function (x, sizeX, sizeY) {
@@ -16,6 +16,8 @@ Raphael.fn.createBocal = function (x, sizeX, sizeY) {
 };
 
 function remplirTabBoule(nbBoule, nbCouleur) {
+    reInit();
+    calculateballs(nbBoule);
     var nbMaxBoules = Math.ceil(nbBoule / nbCouleur), gen = [];
     for (var i = 0; i < nbBoule; i++) {
         var numColor = Math.floor(Math.random() * nbCouleur);
@@ -28,25 +30,21 @@ function remplirTabBoule(nbBoule, nbCouleur) {
         }
     }
     tabBoule.push(gen);
-    console.log(tabBoule);
-    console.log(tabListBallByColors);
     viewBalls();
 }
+function tabNewGen() {
 
-function newGen() {
-    var nbBoule = tabBoule[0].length, gen = [];
-    console.log(nbBoule);
-    for (var i = 0; i < nbBoule; i++) {
-        var numColor = tabBoule[nbGen][Math.floor(Math.random() * nbBoule)];
+    gen = [];
+    for (var i = 0; i < values[0]; i++) {
+        var numColor = tabBoule[nbGen][Math.floor(Math.random() * values[0])];
         gen.push(numColor);
     }
     tabBoule.push(gen);
-    console.log(tabBoule);
     nbGen++;
     document.getElementById('gennumberst').innerHTML = nbGen - 1;
     document.getElementById('gennumbersd').innerHTML = nbGen;
+    viewBalls(true);
 }
-
 function reInit() {
     ra.clear();
     ra.createBocal(0, 300, 300);
@@ -57,17 +55,11 @@ function reInit() {
     document.getElementById('gennumbersd').innerHTML = 1;
     nbGen = 0;
 }
-
-function viewBalls() {
-    values = calculateballs();
-    //values[0] -> nombre de boules
-    //values[1] -> nombre de boules par lignes
-    //values[2] -> rayon d'une boule
-    console.log('nombre de boules:' + values[0]);
-    console.log('nombre de boules par lignes:' + values[1]);
-    console.log('rayon:' + values[2]);
-
-    positionX = 100, positionY = 300, ballsAdd = 0;
+function viewBalls(newGenTF, cooef) {
+    cooef = cooef || 0;
+    nbGenBalls = nbGen+cooef;
+    if (newGenTF) {positionX = 450}else{positionX = 100}
+     positionY = 300, ballsAdd = 0;
     for (var iY = 0; iY < values[0]; iY++) {
         for (var iX = 0; iX < values[1]; iX++) {
             if (ballsAdd == values[0]) {
@@ -75,32 +67,23 @@ function viewBalls() {
                 iY = values[0];
             }
             else {
-                ra.ball(positionX, positionY, values[2], tabBoule[nbGen][ballsAdd]);
+                ra.ball(positionX, positionY, values[2], tabBoule[nbGenBalls][ballsAdd]);
                 positionX = positionX + (2 * values[2]);
                 ballsAdd++;
+
             }
         }
-        positionX = 100;
+        if (newGenTF) {positionX = 450}else{positionX = 100}
         positionY = positionY - (2 * values[2]);
     }
 }
+function calculateballs(nbBoule) {
 
-function calculateballs() {
-    nbBoule = tabBoule[0].length;
     bouleParLigne = Math.ceil(Math.sqrt(nbBoule));
     rayonBoule = Math.floor(300 / bouleParLigne / 2.5);
-
-    value = [nbBoule, bouleParLigne, rayonBoule];
-    return value;
+    values = [nbBoule, bouleParLigne, rayonBoule];
 }
-
-function pullBall() {
-
-}
-
 window.onload = function () {
     //creation du cadre
     reInit();
-
-    console.log(nbGen);
 }
